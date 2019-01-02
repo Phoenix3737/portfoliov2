b2b9132a1e89ae958c3c93ffddeb4e6bce7426b9
mark groves <groves999@gmail.com>
2019-01-02 17:44:38 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+import Testimonials from  './components/testimonials/testimonials';
+import ContactUs from './components/contact/contact';
+import Footer from './components/footer/footer';
+import resumeData from './components/resume/resumeData.js';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Header resumeData={resumeData}/>
+//       </div>
+//     );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData} />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <ContactUs />
+        <Footer />
+    </div>
+    );
+  }
+}
+export default App;
