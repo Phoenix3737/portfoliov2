c58b5b4ad2db94e3dd20f1a68c9964f2e5bd5613
mark groves <groves999@gmail.com>
2019-01-04 08:11:51 -0500

    Updates

---
+++ precache-manifest.62e93226cbe3dc9fac42ef3357d761f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98256e333b889b9119c0",
+    "url": "/portfoliov2/static/js/runtime~main.98256e33.js"
+  },
+  {
+    "revision": "757fea544cc1da85ad48",
+    "url": "/portfoliov2/static/js/main.757fea54.chunk.js"
+  },
+  {
+    "revision": "739e6e4ba4244221c928",
+    "url": "/portfoliov2/static/js/1.739e6e4b.chunk.js"
+  },
+  {
+    "revision": "757fea544cc1da85ad48",
+    "url": "/portfoliov2/static/css/main.9a28516d.chunk.css"
+  },
+  {
+    "revision": "8b0e0787645420d1fb9add85d8ce6f13",
+    "url": "/portfoliov2/index.html"
+  }
+];
